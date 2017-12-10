d2349e0747666c164a253c6c5a46d2e57de36d26
wlp <1328855616@qq.com>
2017-12-10 11:27:42 +0800

    完成二级分类的模态框数据显示提交

--- public/manage-my/js/categorySecond.js
+++ public/manage-my/js/categorySecond.js
@@ -1,4 +1,28 @@
 $(function(){
+    //将一级分类的名称显示在模态框中
+    $.ajax({
+        url:"/category/queryTopCategoryPaging",
+        data:{
+            page:1,
+            pageSize:250
+        },
+        success:function(backdata){
+            // console.log(backdata)
+            $('.dropdown-menu').html('')
+            $.each(backdata.rows,function(i,n){
+                console.log(n)
+                var $li="<li><a href='javascript:void(0)'>"+n.categoryName+"</a></li>"
+                $('.dropdown-menu').append($li)
+            })
+        }
+    })
+    //给里面的a标签添加点击事件，点击的时候将内容显示在上面
+    $('.dropdown-menu').on('click','a',function(){
+        var text=$(this).html();
+        console.log(text)
+        $('.selecttext').html(text)
+    })
+
     var mypage=1;
     var mypageSize=5;
     //封装页面加载数据的函数
@@ -10,7 +34,7 @@ $(function(){
                 pageSize:mypageSize
             },
             success:function(backdata){
-                console.log(backdata)
+                // console.log(backdata)
                 $('tbody').html(template('secondTmp',backdata))
     
                 $("#pagintor").bootstrapPaginator({
@@ -35,6 +59,20 @@ $(function(){
         e.preventDefault()
         console.log($('form').serialize())
     })
+    //点击上传文件按钮，上传文件
+    $("#fileupload").fileupload({
+        dataType:"json",
+        //e：事件对象
+        //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+        done:function (e, data) {
+        //   console.log(data);
+        //   console.log(data.result.picAddr)
+        $('.imgs').attr('src',data.result.picAddr);
+        }
+      });
+
+      
+      
    
 
    
